6af8bb229769e6e0af9b2f6067f58c7b5279d2e9
lcarlossandberg <carlossandberg@googlemail.com>
2016-12-23 18:01:40 +0000

    Assignment1 v1.0.2

--- code.py
+++ code.py
@@ -5,7 +5,6 @@ from matplotlib import image as img
 
 
 
-
 class Greengraph(object):
     def __init__(self, start, end):
         self.start=start
@@ -25,35 +24,34 @@ class Greengraph(object):
 
     def green_between(self, steps):
         return [Map(*location).count_green()
-                for location in self.location_sequence(
-                    self.geolocate(self.start),
-                    self.geolocate(self.end),
-                    steps)]
+                for location in self.location_sequence(self.geolocate(self.start), self.geolocate(self.end),steps)]
+
 
 
 class Map(object):
     def __init__(self, lat, long, satellite=True,zoom=10, size=(400,400), sensor=False):
         base="http://maps.googleapis.com/maps/api/staticmap?"
-
+        
         params=dict(
-                    sensor= str(sensor).lower(),
-                    zoom= zoom,
-                    size= "x".join(map(str, size)),
-                    center= ",".join(map(str, (lat, long) )),
-                    style="feature:all|element:labels|visibility:off"
-                    )
-
+            sensor= str(sensor).lower(),
+            zoom= zoom,
+            size= "x".join(map(str, size)),
+            center= ",".join(map(str, (lat, long) )),
+            style="feature:all|element:labels|visibility:off"
+        )
+            
         if satellite:
             params["maptype"]="satellite"
-
-        self.image = requests.get(base, params=params).content # Fetch our PNG image data
-
+                    
+        self.image = requests.get(base, params=params).content
+        # Fetch our PNG image data
+    
         self.pixels= img.imread(StringIO(self.image))
         # Parse our PNG image as a numpy array
 
 
     def green(self, threshold):
-    # Use NumPy to build an element-by-element logical array
+        # Use NumPy to build an element-by-element logical array
         greener_than_red = self.pixels[:,:,1] > threshold* self.pixels[:,:,0]
         greener_than_blue = self.pixels[:,:,1] > threshold*self.pixels[:,:,2]
         green = np.logical_and(greener_than_red, greener_than_blue)
@@ -71,13 +69,12 @@ class Map(object):
         result = img.imsave(buffer, out, format='png')
         return buffer.getvalue()
 
-    mygraph=Greengraph('New York','Chicago')
-    data = mygraph.green_between(20)
-    plt.plot(data)
 
 
 
-
+mygraph=Greengraph('New York','Chicago')
+data = mygraph.green_between(20)
+plt.plot(data)
 
 
 
