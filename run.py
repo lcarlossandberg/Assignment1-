e078fb16f67bab39ad297a4247f60938d18c3fbd
lcarlossandberg <carlossandberg@googlemail.com>
2016-12-27 09:25:46 +0000

    Assignment1 V1.2.0

---
+++ run.py
@@ -0,0 +1,17 @@
+from matplotlib import image as img
+from matplotlib import pyplot as plt
+from graph import Greengraph
+
+
+mygraph=Greengraph('New York','Chicago')
+data = mygraph.green_between(20)
+plt.plot(data)
+plt.show()
+
+
+
+
+
+
+
+
