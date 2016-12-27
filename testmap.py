044bfde704407b3a0dc4663865b54cb00a22605c
lcarlossandberg <carlossandberg@googlemail.com>
2016-12-27 21:18:28 +0000

    Assignment1 V2.3.1

--- testmap.py
+++ testmap.py
@@ -5,6 +5,8 @@ import geopy
 import requests
 from io import BytesIO
 from matplotlib import image as img
+import os
+import numpy as np
 
 def test_init():#have an error which shouldnt happen as its getting mocked, made similar to notes still error
     with patch.object(requests,'get') as mock_get:
@@ -22,5 +24,13 @@ def test_init():#have an error which shouldnt happen as its getting mocked, made
                                         }
                                         )
 
+def test_green():
+    mock_map = open(os.path.join('London.png'),'rb')
+    mock_pixel = np.load(os.path.join('LondonData.npy'))
+    threshold = 1.1
+    with patch('requests.get', return_value=Mock(content=mock_map.read())) as mock_get:
+         test_Map = Map(51.5074,-0.1278)
+         assert (test_Map.green(threshold) == mock_pixel).all() == True
 
 #test_init()
+#test_green()
