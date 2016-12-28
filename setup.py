187c6d83a00e474080d5f21fb92da27ac3999a39
lcarlossandberg <carlossandberg@googlemail.com>
2016-12-28 13:48:16 +0000

    Assignment1 V2.5.4

--- setup.py
+++ setup.py
@@ -2,13 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
       name = "greengraph",
-      version = "2.4.3",
+      version = "3",
       packages = find_packages(exclude=['*test']),
       scripts = ['scripts/greengraph'],
       
       install_requires = ['argparse','numpy','geopy','requests','matplotlib']
       
-      author = "Leo Carlos-Sandberg",
-      description = "Meausres amount of green pixels in google maps",
-      license = "MIT",
 )
